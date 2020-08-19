a69929b3ee08febb975ed3b9573ab436854c7bc4
Adrian Bielawski <adrian@bielaw.ski>
2020-08-19 10:02:53 +0100

    Fetch user info when My account clicked

--- src/js/components/game/side_menu/menu_content/menuContent.js
+++ src/js/components/game/side_menu/menu_content/menuContent.js
@@ -1,34 +1,12 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { db } from 'firebaseConfig';
+import React from 'react';
 import styles from './menuContent.scss'
 //Custom Components
 import GameId from 'components/global_components/game_id/gameId';
 import LogOut from 'components/global_components/accountInfo/logout';
 import MyAccount from 'components/global_components/accountInfo/my_account/myAccount';
-//Redux actions
-import { setUserInfo, setFetchingUserInfo } from 'actions/sideMenuActions';
-import LoadingSpinner from 'components/global_components/loading_spinner/loadingSpinner';
 
 const MenuContent = (props) => {
-    useEffect(() => {
-        db.collection('users').doc(props.user.uid).get()
-            .then((response) => {
-                let data = response.data();
-                const allGames = data.allGames;
-                let reversedAllGames = [];
-                for (let i = 0; i < allGames.length; i++) {
-                    reversedAllGames.unshift(allGames[i]);
-                }
-                data.allGames = reversedAllGames;
-
-                props.setUserInfo(data);
-                props.setFetchingUserInfo(false);
-            });
-    }, [])
-
     return (
-        props.fetchingUserInfo ? <LoadingSpinner background={false} /> : 
         <div className={styles.menuContent}>
             <LogOut className={styles.logout} />
             <MyAccount />
@@ -37,18 +15,4 @@ const MenuContent = (props) => {
     );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        fetchingUserInfo: state.sideMenu.fetchingUserInfo,
-        user: state.app.user,
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setUserInfo: (userInfo) => dispatch(setUserInfo(userInfo)),
-        setFetchingUserInfo: (fetchingUserInfo) => dispatch(setFetchingUserInfo(fetchingUserInfo)),
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(MenuContent);
+export default MenuContent;
--- src/js/components/global_components/accountInfo/my_account/myAccount.js
+++ src/js/components/global_components/accountInfo/my_account/myAccount.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
+import { db } from 'firebaseConfig';
 import styles from './myAccount.scss'
 //Custom Components
 import GamesHistory from './games_history/gamesHistory';
 //Redux actions
-import { setShowAccountInfo, setShowAccountSettings } from 'actions/sideMenuActions';
+import { setUserInfo, setFetchingUserInfo, setShowAccountInfo, setShowAccountSettings } from 'actions/sideMenuActions';
 
 const MyAccount = (props) => {
     const { t } = useTranslation();
   
     const showMyAccount = () => {
-        props.setShowAccountInfo(!props.showAccountInfo);
+        if (!props.userInfo) {
+            db.collection('users').doc(props.user.uid).get()
+                .then((response) => {
+                    let data = response.data();
+                    const allGames = data.allGames;
+                    let reversedAllGames = [];
+                    for (let i = 0; i < allGames.length; i++) {
+                        reversedAllGames.unshift(allGames[i]);
+                    }
+                    data.allGames = reversedAllGames;
+
+                    props.setUserInfo(data);
+                    props.setFetchingUserInfo(false);
+                    props.setShowAccountInfo(!props.showAccountInfo);
+                })
+        } else {
+            props.setShowAccountInfo(!props.showAccountInfo);
+        }
     }
   
     const showAccountSettings = () => {
@@ -21,17 +39,22 @@ const MyAccount = (props) => {
     return (
         <div className={styles.myAccount}>
             <p className={styles.userName} onClick={showMyAccount}>{t("My account")}</p>
+            {!props.fetchingUserInfo && 
             <div className={`${styles.accountContent} ${props.showAccountInfo && styles.showContent}`}
                 style={props.showGames && props.showAccountInfo ? {maxHeight: '300px'} : null}>
                 <p onClick={showAccountSettings}>{t("Account settings")}</p>
                 <GamesHistory />
             </div>
+            }
         </div>
     );
 }
 
 const mapStateToProps = (state) => {
     return {
+        fetchingUserInfo: state.sideMenu.fetchingUserInfo,
+        user: state.app.user,
+        userInfo: state.sideMenu.userInfo,
         showAccountInfo: state.sideMenu.showAccountInfo,
         showGames: state.sideMenu.showGames,
         showAccountSettings: state.sideMenu.showAccountSettings,
@@ -40,6 +63,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        setUserInfo: (userInfo) => dispatch(setUserInfo(userInfo)),
+        setFetchingUserInfo: (fetchingUserInfo) => dispatch(setFetchingUserInfo(fetchingUserInfo)),
         setShowAccountInfo: (showAccountInfo) => dispatch(setShowAccountInfo(showAccountInfo)),
         setShowAccountSettings: (showAccountSettings) => dispatch(setShowAccountSettings(showAccountSettings)),
     }
