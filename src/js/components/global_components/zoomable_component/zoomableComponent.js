6b75bed59e555506e43b7fcce37baa4c60b4682c
Adrian Bielawski <adrian@bielaw.ski>
2020-08-25 18:32:45 +0100

    ZoomableComponent refactored

--- src/js/components/global_components/zoomable_component/zoomableComponent.js
+++ src/js/components/global_components/zoomable_component/zoomableComponent.js
@@ -1,73 +1,56 @@
 import React, { useState, useCallback, useRef } from 'react';
+import { clamp } from 'lodash';
 import styles from './zoomableComponent.scss';
 
 const ZoomableComponent = (props) => {
-    const zoomableComponent = useRef(null);
+    const wrapper = useRef(null);
     const content = useRef(null);
     const [startDistance, setStartDistance] = useState(null);
     const [startScale, setStartScale] = useState(1);
     const [scale, setScale] = useState(1);
-    const [startMove, setStartMove] = useState({x: 0, y: 0});
-    const [translate, setTranslate] = useState({x: 0, y: 0});
-    const [maxTranslateX, setMaxTranslateX] = useState(null);
-    const [maxTranslateY, setMaxTranslateY] = useState(null);
-    const [minTranslateX, setMinTranslateX] = useState(null);
-    const [minTranslateY, setMinTranslateY] = useState(null);
-    
+    const [midPoint, setMidPoint] = useState({ x: 0, y: 0 })
+    const [startMove, setStartMove] = useState({ x: 0, y: 0 });
+    const [translate, setTranslate] = useState({ x: 0, y: 0 });
+    const [translateClamp, setTranslateClamp] = useState(null);
+    const margin = 5;
+
     const handleTouchStart = useCallback((e) => {
         if (e.touches.length === 1) {
             const x = e.touches[0].clientX;
             const y = e.touches[0].clientY;
 
-            setStartMove({x, y});
-            const zoomableComponentD = zoomableComponent.current.getBoundingClientRect();
-            const contentD = content.current.getBoundingClientRect();
-
-            const maxX = zoomableComponentD.x - contentD.x + translate.x + 5;
-            const maxY = zoomableComponentD.y - contentD.y + translate.y + 5;
-            const minX = zoomableComponentD.x + zoomableComponentD.width - contentD.x - contentD.width + translate.x - 5;
-            const minY = zoomableComponentD.y + zoomableComponentD.height - contentD.y - contentD.height + translate.y - 5;
-            setMaxTranslateX(maxX);
-            setMaxTranslateY(maxY);
-            setMinTranslateX(minX);
-            setMinTranslateY(minY);
+            setStartMove({ x, y });
+            updateTranslateClamp();
         }
     })
 
     const handleTouchMove = useCallback((e) => {
         if (e.touches.length === 1 && scale > 1 && startMove) {
-
             const x = e.touches[0].clientX;
             const y = e.touches[0].clientY;
 
-            let newTranslateX = x - startMove.x + translate.x;
-            let newTranslateY = y - startMove.y + translate.y;
+            const newTranslateX = clamp(
+                x - startMove.x + translate.x,
+                translateClamp.minX,
+                translateClamp.maxX
+            );
 
-            if (newTranslateX >= maxTranslateX) {
-                newTranslateX = maxTranslateX
-            }
+            const newTranslateY = clamp(
+                y - startMove.y + translate.y,
+                translateClamp.minY,
+                translateClamp.maxY
+            );
 
-            if (newTranslateY >= maxTranslateY) {
-                newTranslateY = maxTranslateY
-            }
-
-            if (newTranslateX <= minTranslateX) {
-                newTranslateX = minTranslateX
-            }
-
-            if (newTranslateY <= minTranslateY) {
-                newTranslateY = minTranslateY
-            }
-
-            setTranslate({x: newTranslateX, y: newTranslateY})
+            setTranslate({ x: newTranslateX, y: newTranslateY })
         }
 
         if (e.touches.length === 2) {
+            const [touch1, touch2] = e.touches;
             const distance = Math.sqrt(
                 Math.pow(
-                    Math.abs(e.touches[0].clientX - e.touches[1].clientX), 2) +
+                    Math.abs(touch1.clientX - touch2.clientX), 2) +
                 Math.pow(
-                    Math.abs(e.touches[0].clientY - e.touches[1].clientY), 2)
+                    Math.abs(touch1.clientY - touch2.clientY), 2)
             );
 
             if (startDistance === null) {
@@ -75,65 +58,54 @@ const ZoomableComponent = (props) => {
                 return;
             }
 
-            let _scale = startScale * distance / startDistance;
-            if (_scale < 1) {
-                _scale = 1;
-            }
-            if (_scale > 3) {
-                _scale = 3;
-            }
-            const zoomableComponentD = zoomableComponent.current.getBoundingClientRect();
-            const zoomableComponentX = zoomableComponentD.left;
-            const zoomableComponentY = zoomableComponentD.top;
+            const wrapperD = wrapper.current.getBoundingClientRect();
 
-            const midPointX = (e.touches[0].clientX + e.touches[1].clientX) / 2 - zoomableComponentX;
-            const midPointY = (e.touches[0].clientY + e.touches[1].clientY) / 2 - zoomableComponentY;
+            const midPointX = clamp(
+                (touch1.clientX + touch2.clientX) / 2 - wrapperD.left,
+                -margin,
+                wrapperD.width - margin * 2
+            )
+            const midPointY = clamp(
+                (touch1.clientY + touch2.clientY) / 2 - wrapperD.top,
+                -margin,
+                wrapperD.height - margin * 2
+            )
+            setMidPoint({ x: midPointX, y: midPointY })
 
-            if (midPointX <= -5) {
-                midPointX = -5;
-            }
-
-            if (midPointY <= -5) {
-                midPointY = -5;
-            }
-
-            if (midPointX >= zoomableComponentD.width - 10) {
-                midPointX = zoomableComponentD.width - 10;
-            }
-
-            if (midPointY >= zoomableComponentD.height - 10) {
-                midPointY = zoomableComponentD.height - 10;
-            }
-
-            content.current.style.transformOrigin = `${midPointX}px ${midPointY}px`;
-            setScale(_scale)
-            if (_scale <= 1) {
-                setTranslate({x: 0, y: 0})
+            const newScale = clamp(startScale * distance / startDistance, 1, 3);
+            setScale(newScale)
+            if (newScale <= 1) {
+                setTranslate({ x: 0, y: 0 })
             }
         }
     }, [startDistance, startMove, scale])
 
-    const handleTouchEnd = useCallback((e) => {
+    const handleTouchEnd = useCallback(() => {
         setStartDistance(null);
         setStartScale(scale);
         setStartMove(null);
-        const zoomableComponentD = zoomableComponent.current.getBoundingClientRect();
-        const contentD = content.current.getBoundingClientRect();
-
-        const maxX = zoomableComponentD.x - contentD.x + translate.x + 5;
-        const maxY = zoomableComponentD.y - contentD.y + translate.y + 5;
-        const minX = zoomableComponentD.x + zoomableComponentD.width - contentD.x - contentD.width + translate.x - 5;
-        const minY = zoomableComponentD.y + zoomableComponentD.height - contentD.y - contentD.height + translate.y - 5;
-        setMaxTranslateX(maxX);
-        setMaxTranslateY(maxY);
-        setMinTranslateX(minX);
-        setMinTranslateY(minY);
+        updateTranslateClamp();
     }, [scale])
 
+    const updateTranslateClamp = () => {
+        const wrapperD = wrapper.current.getBoundingClientRect();
+        const contentD = content.current.getBoundingClientRect();
+
+        const maxX = wrapperD.x - contentD.x + translate.x + margin;
+        const maxY = wrapperD.y - contentD.y + translate.y + margin;
+        const minX = maxX + wrapperD.width - contentD.width - margin * 2;
+        const minY = maxY + wrapperD.height - contentD.height - margin * 2;
+        setTranslateClamp({ maxX, maxY, minX, minY });
+    }
+
     return (
-        <div className={`${styles.zoomableComponent} ${props.className}`} ref={zoomableComponent}>
-            <div className={props.contentClassName}
-                style={{transform: `translate(${translate.x}px, ${translate.y}px) scale(${scale})`}}
+        <div className={`${styles.wrapper} ${props.className}`} ref={wrapper}>
+            <div
+                className={props.contentClassName}
+                style={{ 
+                    transform: `translate(${translate.x}px, ${translate.y}px) scale(${scale})`,
+                    transformOrigin: `${midPoint.x}px ${midPoint.y}px`
+                }}
                 onTouchStart={handleTouchStart}
                 onTouchMove={handleTouchMove}
                 onTouchEnd={handleTouchEnd}
