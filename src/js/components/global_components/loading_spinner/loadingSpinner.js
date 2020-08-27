9ea8cb0d5ef2f34356a816b37cc491d087b5d885
Adrian Bielawski <adrian@bielaw.ski>
2020-08-27 11:46:06 +0100

    Size prop added to LoadingSpinner component

--- src/js/components/global_components/loading_spinner/loadingSpinner.js
+++ src/js/components/global_components/loading_spinner/loadingSpinner.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './loadingSpinner.scss'
 
-const LoadingSpinner = (props) => <div className={`${styles.loader} ${!props.background && styles.noBackground}`}>Loading...</div>;
+const LoadingSpinner = (props) => 
+    <div
+        className={`${styles.loader} ${!props.background && styles.noBackground}`}
+        style={{height: `${props.size}px`, width: `${props.size}px`}}
+    >
+        Loading...
+    </div>;
 
 export default LoadingSpinner;
