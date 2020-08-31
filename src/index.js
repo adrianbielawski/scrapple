181c12186235a6db3049d85f86d56fb05b2e4b51
Adrian Bielawski <adrian@bielaw.ski>
2020-08-31 10:43:23 +0100

    muiTheme moved to hoc folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'reducers/';
-import theme from 'components/global_components/muiTheme';
+import theme from 'hoc/muiTheme';
 import { ThemeProvider } from '@material-ui/core';
 
 let composeEnhancers = compose;
