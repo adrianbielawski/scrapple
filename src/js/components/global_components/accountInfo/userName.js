6fa1591703274c7c0f1eec88c4221d6b089069cf
Adrian Bielawski <adrian@bielaw.ski>
2020-09-03 17:18:56 +0100

    UserName component refactored

--- src/js/components/global_components/accountInfo/userName.js
+++ src/js/components/global_components/accountInfo/userName.js
@@ -6,12 +6,13 @@ import { useTranslation } from 'react-i18next';
 const UserName = (props) => {
   const { t } = useTranslation();
 
-  return <p>{t("Logged in as", { 'name': props.userName })}</p>;
+  return <p>{t("Logged in as", { 'name': `${props.firstName} ${props.lastName}` })}</p>;
 }
 
 const mapStateToProps = (state) => {
   return {
-    userName: state.app.user.displayName
+    firstName: state.app.user.first_name,
+    lastName: state.app.user.last_name,
   }
 }
 
