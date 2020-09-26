69b354e49d67d937a51eb4e35435817813829df3
Adrian Bielawski <adrian@bielaw.ski>
2020-09-26 16:00:40 +0100

    SubtractPoints refactored

--- src/js/actions/subtractPointsActions.js
+++ src/js/actions/subtractPointsActions.js
@@ -1,19 +1,11 @@
-import db from 'firebaseConfig';
-import { cloneDeep } from 'lodash';
+import axiosInstance from 'axiosInstance';
+import { setAlert } from 'actions/appActions';
 
-export const subPoints = (gameId, players, points, history) => {
-    return () => {
-        const updatedPlayers = cloneDeep(players);
-
-        for (const player of updatedPlayers) {
-            player.currentScore -= points[player.playerIndex] || 0;
-        }
-
-        db.collection('games').doc(gameId).update({
-            players: updatedPlayers,
-            pointsSubtracted: true
-        }).then(() => {
-            history.push(`/game/${gameId}/game_summary`);
-        });
-    }
+export const subPoints = (gameId, points) => dispatch => {
+    axiosInstance.put(`/games/${gameId}/subtract-points/`, {
+        points,
+    })
+    .catch(() => {
+        dispatch(setAlert('alert', 'Something went wrong'));
+    })
 };
