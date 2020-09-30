2318ca3572b931a27991029e0d65d472c4656792
Adrian Bielawski <adrian@bielaw.ski>
2020-09-30 18:07:25 +0100

    Display modal bug fixed, code cleaned by adding classnames

--- src/js/components/global_components/modal/modal.js
+++ src/js/components/global_components/modal/modal.js
@@ -1,22 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import Card from '../card/card';
+import classNames from 'classnames/bind';
 import styles from './modal.scss';
+//Custom components
+import Card from '../card/card';
 
 const Modal = (props) => {
-    const [cardClass, setCardClass] = useState(null);
+    const [showCard, setShowCard] = useState(null);
+    const [top, setTop] = useState(0);
+
+    const updatePosition = () => {
+        setTop(window.pageYOffset);
+    }
+
     useEffect(() => {
-        setCardClass(styles.show)
+        setShowCard(styles.show)
     }, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', updatePosition);
+
+        return () => {
+            window.removeEventListener('scroll', updatePosition)
+        }
+    }, [])
+
+    const cx = classNames.bind(styles);
+    const modalClass = cx('modal', props.className);
+    const cardClass = cx(
+        'card',
+        props.cardClassName,
+        {
+            [styles.show]: showCard
+        }
+    );
+
     return (
         props.show && ReactDOM.createPortal(
-        <div className={`${styles.modal} ${props.className || ''}`}>
-            <div className={styles.wrapper}>
-                <Card className={`${styles.card} ${cardClass} ${props.cardClassName || ''}`}>
-                    {props.children}
-                </Card>
-            </div>
-        </div>, document.body)
+            <div className={modalClass} style={{ top }}>
+                <div className={styles.wrapper}>
+                    <Card className={`${styles.card} ${cardClass} ${props.cardClassName || ''}`}>
+                        {props.children}
+                    </Card>
+                </div>
+            </div>, document.body
+        )
     );
 }
 
